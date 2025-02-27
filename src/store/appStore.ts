f495a656623dc1e9f3e26cd88554ffdd65de50e0
Abhishek Sharma <abhish3kk@gmail.com>
2025-02-27 22:35:25 +0530

    use store

---
+++ src/store/appStore.ts
@@ -0,0 +1,21 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useAppStore = defineStore(
+  "appStore",
+  () => {
+    const theme = ref<string>();
+    const setTheme = (value: string) => {
+      theme.value = value;
+      document.documentElement.setAttribute("data-theme", value);
+    };
+
+    return {
+      theme,
+      setTheme,
+    };
+  },
+  {
+    persist: true,
+  },
+);
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export enum APP_THEMES {
+  DARK = "dark",
+  LIGHT = "light",
+}
